1f9d2ed4f171ae6a138990821843b5b0c46f36f2
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-07-10 17:35:04 +0200

    robotpkg management command: only look for */{slug} and */py-{slug}

--- rainboard/management/commands/robotpkg.py
+++ rainboard/management/commands/robotpkg.py
@@ -1,3 +1,4 @@
+import itertools
 import logging
 
 from django.conf import settings
@@ -20,7 +21,7 @@ class Command(BaseCommand):
 
         for project in Project.objects.all():
             for slug in [project.slug, project.slug.replace('_', '-')]:
-                for pkg in path.glob(f'*/*{slug}'):
+                for pkg in itertools.chain(path.glob(f'*/{slug}'), path.glob(f'*/py-{slug}')):
                     obj, created = Robotpkg.objects.get_or_create(name=pkg.name, category=pkg.parent.name,
                                                                   project=project)
                     if created:
