5c6a92b866f042a11ea33f547c1c04b51ae03ed9
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-07-31 15:25:33 +0200

    fix contributors filter for django_filters 2

--- rainboard/filters.py
+++ rainboard/filters.py
@@ -12,9 +12,9 @@ class ProjectFilter(django_filters.FilterSet):
 
 
 class ContributorFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(name='contributorname__name', label='name', lookup_expr='icontains')
-    mail = django_filters.CharFilter(name='contributormail__mail', label='mail', lookup_expr='icontains')
-    project = django_filters.CharFilter(name='projects__name', label='project', lookup_expr='icontains')
+    name = django_filters.CharFilter(field_name='contributorname__name', label='name', lookup_expr='icontains')
+    mail = django_filters.CharFilter(field_name='contributormail__mail', label='mail', lookup_expr='icontains')
+    project = django_filters.CharFilter(field_name='projects__name', label='project', lookup_expr='icontains')
 
     class Meta:
         model = models.Contributor
