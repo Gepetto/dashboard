a1840c3b5a3f5146f9e900bf9106614a2ca00b5e
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-01-17 19:35:42 +0100

    remove laas perms middleware, everything is broken

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'dashboard.middleware.LAASPermsMiddleware',
+    # 'dashboard.middleware.LAASPermsMiddleware',
 ]
 
 ROOT_URLCONF = f'{PROJECT}.urls'
