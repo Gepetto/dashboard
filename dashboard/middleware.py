f81c6578d4fcad7e66dac6798ac5c5ccf7d2a592
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-01-20 17:46:19 +0100

    test

--- dashboard/middleware.py
+++ dashboard/middleware.py
@@ -8,6 +8,8 @@ from rest_framework import permissions
 
 def ip_laas(request: HttpRequest) -> bool:
     forwarded_for = ip_address(request.META.get('HTTP_X_FORWARDED_FOR'))
+    for net in settings.LAAS_NETWORKS:
+        print(forwarded_for, 'in', ip_network(net))
     return any(forwarded_for in ip_network(net) for net in settings.LAAS_NETWORKS)
 
 
@@ -16,11 +18,13 @@ class LAASPermsMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
+        print(request.path.startswith('/admin/'), 'or', request.path.startswith('/accounts/'), 'or', request.user,
+              'and', request.user.is_authenticated, 'or', request.method, 'in', permissions.SAFE_METHODS, 'and',
+              ip_laas(request))
 
         allowed = (request.path.startswith('/admin/') or request.path.startswith('/accounts/')
                    or request.user and request.user.is_authenticated
                    or request.method in permissions.SAFE_METHODS and ip_laas(request))
+        print('allowed', allowed)
 
-        if allowed:
-            return self.get_response(request)
         return HttpResponseForbidden()
