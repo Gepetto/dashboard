22e46562169a20456a916c5b969caacdd91918b1
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-05-20 10:59:54 +0200

    format

--- rainboard/migrations/0068_alter_project_clang_default.py
+++ rainboard/migrations/0068_alter_project_clang_default.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rainboard', '0067_project_clang_default'),
+        ("rainboard", "0067_project_clang_default"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='project',
-            name='clang_default',
+            model_name="project",
+            name="clang_default",
             field=models.BooleanField(default=True),
         ),
     ]
