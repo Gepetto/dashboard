eb3afcef66a3e3d2c60efae6f3ba9f3ba250778e
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-08-11 13:04:28 +0200

    add i18n urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("gh/", include("gh.urls")),
     path("", include("rainboard.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
