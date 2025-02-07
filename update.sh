90ff9fb39529abf190ad1ef37c19e4db6d61a72f
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-07 15:08:29 +0100

    docker-compose -> docker compose

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 git pull
-docker-compose pull
-docker-compose up -d
+docker compose pull
+docker compose up -d
 docker system prune -f
-docker-compose logs -f --tail 20
+docker compose logs -f --tail 20
